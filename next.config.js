1db4b62dbe942bac2831dc8c6c9d0e004abbbcc3
RoughGhost <stephendlouis6@gmail.com>
2022-05-16 17:24:00 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'encrypted-tbn0.gstatic.com',
+      'cdn.sanity.io',
+      's3.coinmarketcap.com',
+      's2.coinmarketcap.com',
+    ],
+  },
 }
