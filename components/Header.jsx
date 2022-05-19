bf95ef4c84771d80909463861488d31b6c876d7b
RoughGhost <stephendlouis6@gmail.com>
2022-05-19 12:14:01 +0100

    updated CMCTable

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Image from 'next/image'
 import Search from '../assets/svg/search'
+import { ConnectButton } from 'web3uikit'
 
 const styles = {
   navLink: `text-white flex mx-[10px]`,
@@ -10,56 +11,66 @@ const styles = {
   header: `bg-[#17171A] text-white h-20 flex gap-[100px] w-full p-[30px]`,
   headerWrapper: `flex justify-center h-full max-w-screen-xl mx-auto px-4`,
   inputContainer: `flex items-center justify-center p-2 rounded bg-[#171924]`,
-  input: `bg-transparent outline-none text-white w-70 ml-3`,
+  input: `bg-transparent outline-none text-white w-20 ml-3`,
   cursorPointer: `mr-5 cursor-pointer`,
 }
 
 const Header = () => {
+  //   const { getQuote } = useContext(CoinMarketContext)
   return (
     <div className={styles.header}>
       <Image
-        alt="coinmarket"
+        alt=""
         src="https://s2.coinmarketcap.com/static/cloud/img/coinmarketcap_white_1.svg"
         width={220}
         height={220}
       />
+
       <div className={styles.headerWrapper}>
         <nav className={styles.nav}>
           <div className={styles.navItem}>
             <div className={styles.navLink}>Cryptocurrencies</div>
             <div className={styles.badge} />
           </div>
-          <div className={styles.navItem}>
+
+          <div className={styles.navItem} onClick={''}>
             <div className={styles.navLink}>Exchanges</div>
           </div>
+
           <div className={styles.navItem}>
             <div className={styles.navLink}>NFT</div>
             <div className={styles.badge} />
           </div>
+
           <div className={styles.navItem}>
             <div className={styles.navLink}>Cryptown</div>
             <div className={styles.badge} />
           </div>
+
           <div className={styles.navItem}>
             <div className={styles.navLink}>Portfolio</div>
           </div>
+
           <div className={styles.navItem}>
             <div className={styles.navLink}>Watchlist</div>
           </div>
+
           <div className={styles.navItem}>
             <div className={styles.navLink}>Products</div>
             <div className={styles.badge} />
           </div>
+
           <div className={styles.navItem}>
             <div className={styles.navLink}>Learn</div>
           </div>
         </nav>
+
         <div className="flex items-center">
-          {/* <ConnectButton /> */}
-          <div className={styles.inputContainer}>
+          <ConnectButton />
+          {/* <div className={styles.inputContainer}>
             <Search />
-            <input className={styles.input} type="text" placeholder="Search" />
-          </div>
+            <input className={styles.input} placeholder="Search" />
+          </div> */}
         </div>
       </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import type { NextPage } from 'next'
 import Header from "../components/Header"
+import Trending from "../components/Trending"
+import CMCTable from "../components/cmc-table/CMCTabel"
 
 const Home: NextPage = () => {
   return (
     <div className="min-h-screen">
       <Header />
       <div className='mt-10' />
-     {/* Trending  */}
+        <Trending />
      <div className='mt-20' />
-     {/* CMCTabel  */}
+     <CMCTable /> 
     </div>
   )
 }
